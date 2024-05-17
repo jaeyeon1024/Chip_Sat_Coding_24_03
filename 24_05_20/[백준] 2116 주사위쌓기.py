e08996a3142c105df644108eda0458695f2f303a
JaeYeon <jaeyeon2001@gmail.com>
2024-05-17 16:25:25 +0900

    [ADD] 백준 2116 주사위 쌓기 문제 해결

--- 24_05_20/[백준] 2116 주사위쌓기.py
+++ 24_05_20/[백준] 2116 주사위쌓기.py
@@ -10,20 +10,31 @@ dices = [list(map(int, input().split())) for _ in range(n)]
 jump_idx_dic = {0: 5, 1: 3, 2: 4, 3: 1, 4: 2, 5: 0}
 
 answer = 0
-# top bottom은 idx or value => idx
+# top bottom은 idx or value => value
 for i in range(6):
-    bottom = i
-    top = jump_idx_dic[bottom]
-    print(dices)
-    answer += max(
+    bottom = dices[0][i]
+    top = dices[0][jump_idx_dic[i]]
+    sums = 0
+    sums += max(
         dices[0],
         key=lambda x: (
-            0 if dices[0].index(x) == top or dices[0].index(x) == bottom else x
+            0 if bottom == x or top == x else x
         ),
     )
-    print(answer)
 
+    for dice in dices[1:]:
+        idx = dice.index(top)
+        bottom = dice[idx]
+        top = dice[jump_idx_dic[idx]]
+        sums += max(
+            dice,
+            key=lambda x: (
+                0 if bottom == x or top == x else x
+            ),
+        )
+    answer = max(answer, sums)
 
+print(answer)
 """
 
 [1,0,0,0,0,1]   
